0f3d45f220f7fdee6fc7b66b3de042c32d3c4a3e
babam6a <babamba@kaist.ac.kr>
2023-11-13 21:21:22 +0900

    add some functions

---
+++ utils.py
@@ -0,0 +1,11 @@
+
+def sentence_similarity(sentence1, sentence2) :
+    import random
+    # temporary value
+    return random.random()
+
+def make_adjacency_graph(graph) :
+    return
+
+def make_graph(json_file) :
+    return
