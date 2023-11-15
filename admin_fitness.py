45ec25fe244fce5ce336293eb3ae212ebfe14726
sillysillyman <sillysillyman.cs@gmail.com>
2023-11-15 17:03:29 +0900

    subtle errors corrected

--- admin_fitness.py
+++ admin_fitness.py
@@ -5,7 +5,7 @@ INF = 2**31
 def get_admin_fitness(graph):
     num_of_nodes = len(graph.nodes)
     num_of_edges = len(graph.edges)
-    optimal_degrees = math.round(num_of_edges/num_of_nodes)
+    optimal_degrees = round(num_of_edges/num_of_nodes) if round(num_of_edges/num_of_nodes) > 1 else 2
     max_degrees = 2*optimal_degrees
 
     sum_of_diffs = 0
@@ -22,6 +22,9 @@ def get_admin_fitness(graph):
 def get_depth_threshold(graph):
     num_of_nodes = len(graph.nodes)
     num_of_edges = len(graph.edges)
-    optimal_degrees = round(num_of_edges/num_of_nodes)
-    
+    optimal_degrees = round(num_of_edges/num_of_nodes) if round(num_of_edges/num_of_nodes) > 1 else 2
+    print(f"num_of_nodes = {num_of_nodes}")
+    print(f"num_of_edges = {num_of_edges}")
+    print(f"optimal_degrees = {optimal_degrees}")
+
     return math.log(num_of_nodes, optimal_degrees)
