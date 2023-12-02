af1aeaaa1e9ca5e3a6d3a98b537daeff5edb91a8
sillysillyman <sillysillyman.cs@gmail.com>
2023-12-02 18:58:25 +0900

    modified fatal error in optimal_degrees

--- admin_fitness.py
+++ admin_fitness.py
@@ -9,22 +9,20 @@ def get_admin_fitness(graph):
         # If transformation is failed, graph is empty.
         return -INF
     num_of_edges = len(graph.edges)
+    num_of_leaves = len([x for x in graph.nodes() if graph.out_degree(x) == 0])
     optimal_degrees = (
-        round(num_of_edges / num_of_nodes)
-        if round(num_of_edges / num_of_nodes) > 1
+        round(num_of_edges / (num_of_nodes - num_of_leaves))
+        if round(num_of_edges / (num_of_nodes - num_of_leaves)) > 1
         else 2
     )
-    max_degrees = 2 * optimal_degrees
+    # max_degrees = 2 * optimal_degrees
 
     sum_of_diffs = 0
 
     for node in graph.nodes:
-        if (len(graph[node])) > max_degrees:
-            return -INF
         sum_of_diffs += abs(optimal_degrees - len(graph[node]))
 
     fitness = 1 - sum_of_diffs / num_of_edges
-
     return fitness
 
 
